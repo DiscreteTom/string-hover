e2426c1c4f790b68203b20dc7591fd3345fb9e1f
DiscreteTom <discrete_tom@outlook.com>
2023-10-14 09:47:03 +0800

    chore: copy code from json-hover

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,93 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Lexer } from "retsac";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const debug = process.env.VSCODE_DEBUG_MODE === "true";
+
+const lexer = new Lexer.Builder()
+  .ignore(Lexer.whitespaces()) // ignore blank characters
+  .define({
+    string: Lexer.stringLiteral(`"`), // double quote string literal
+    number: /^-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/,
+  })
+  .define(Lexer.wordKind("true", "false", "null")) // type's name is the literal value
+  .anonymous(Lexer.exact(..."[]{},:")) // single char borders
+  .build({ debug });
+
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.languages.registerHoverProvider("json", {
+      provideHover(document, position, token) {
+        const text = document.getText();
+        const offset = document.offsetAt(position);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "string-hover" is now active!');
+        if (debug) {
+          console.log(`offset: ${offset}`);
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('string-hover.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from string-hover!');
-	});
+        lexer.reset().feed(text);
 
-	context.subscriptions.push(disposable);
+        // perf: jump to the start of the target line, instead of lexing the whole file
+        // since JSON doesn't allow multi-line string
+        const lineStartOffset = document.offsetAt(
+          new vscode.Position(position.line, 0)
+        );
+        if (lineStartOffset > 0) {
+          lexer.take(lineStartOffset);
+          if (debug) {
+            console.log(`lineStartOffset: ${lineStartOffset}`);
+          }
+        }
+
+        while (true) {
+          const token = lexer.lex();
+          if (token === null) {
+            break;
+          }
+
+          if (debug) {
+            console.log(token);
+          }
+
+          if (
+            token.kind === "string" &&
+            token.start <= offset &&
+            token.start + token.content.length >= offset
+          ) {
+            // don't show hover if the string is not escaped
+            if (token.content.indexOf("\\") === -1) {
+              if (debug) {
+                console.log(`unescaped string: ${token.content}}`);
+              }
+              return;
+            }
+
+            // escape '`' for markdown code block
+            const longestMatchLength =
+              token.content
+                .match(/`+/g)
+                ?.reduce((a, b) => (a.length > b.length ? a : b)).length ?? 0;
+            const codeblockBorderLength =
+              longestMatchLength >= 3 ? longestMatchLength + 1 : 3;
+
+            // the markdown result
+            const md = [
+              `${"`".repeat(codeblockBorderLength)}txt`,
+              `${JSON.parse(token.content)}`,
+              `${"`".repeat(codeblockBorderLength)}`,
+            ].join("\n");
+
+            if (debug) {
+              console.log(md);
+            }
+
+            return {
+              contents: [md],
+            };
+          }
+        }
+      },
+    })
+  );
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
